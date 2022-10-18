308a55f5bddb4817c06c23ce52d073acd760ced0
KatherineStone <kathyastone@yahoo.com>
2022-10-17 21:02:02 -0400

    adding where we left off at end of monday 10-17

--- script.js
+++ script.js
@@ -1,33 +1,66 @@
+const quotes = [
+    'Things are only impossible until they are not',
+    'Without freedom of choice there is no creativity',
+    'Logic is the beginning of wisdom, not the end',
+];
+
 const quote = document.getElementById('quote');
 const input = document.getElementById('typed-value');
 const start = document.getElementById('start');
+const message = document.getElementById('message');
 
 let wordQueue;
 let highlightPosition;
+let startTime;
 
 
 function startGame(){
     console.log("Game started");
+    document.body.className = "";
+    start.className = "started";
 
-    quoteText = "type me";
+    const quoteIndex = Math.floor(Math.random() * quotes.length);
+    const quoteText = quotes[quoteIndex];
+
+    // quoteText = "type me";
     wordQueue = quoteText.split(' ');
     quote.innerHTML = wordQueue.map(word => (`<span>${word}</span>`)).join('');
 
     highlightPosition = 0;
     quote.childNodes[highlightPosition].className = 'highlight';
+
+    startTime = new Date().getTime();
 }
 start.addEventListener('click', startGame);
 input.addEventListener('input', checkInput);
 
 function checkInput() {
     console.log("Checking", input.value);
-    const currentWord = wordQueue[0];
+    const currentWord = wordQueue[0].replaceAll(".", "").replaceAll(",", "");
     const typedValue = input.value.trim();
-    
+
     if(currentWord !== typedValue){
         input.className = currentWord.startsWith(typedValue) ? "" : "error";
         return;
     }
     wordQueue.shift();
     input.value = "";
+
+    quote.childNodes[highlightPosition].className = "";
+
+    if(wordQueue.length === 0){
+        gameOver();
+        return;
+    }
+    highlightPosition++;
+    quote.childNodes[highlightPosition].className = 'highlight';
+}
+
+function gameOver(){
+    const elapsedTime = new Date().getTime() - startTime;
+    document.body.className = "winner";
+    message.innerHTML = `<span class="congrats">Congratulations!</span>
+    <br>
+    You finished in ${elapsedTime/1000} seconds`;
+
 }
