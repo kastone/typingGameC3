a6ffad36d995fd3ef328d28a6d76272178e68f5e
KatherineStone <kathyastone@yahoo.com>
2022-10-19 21:09:19 -0400

    todo app concepts in the typing game. Needs more styling love.

--- script.js
+++ script.js
@@ -8,14 +8,27 @@ const quote = document.getElementById('quote');
 const input = document.getElementById('typed-value');
 const start = document.getElementById('start');
 const message = document.getElementById('message');
+const gamerName = document.getElementById("gamer-name");
+const scores = getScores();
+const scoresUnorderedList = document.getElementById("scores-unordered-list");
 
 let wordQueue;
 let highlightPosition;
 let startTime;
 
+start.addEventListener('click', startGame);
+input.addEventListener('input', checkInput);
+
 
 function startGame(){
     console.log("Game started");
+    const scoreItem = {
+        name: gamerName.value,
+        milliseconds: 0
+    };
+
+    scores.push(scoreItem);
+
     document.body.className = "";
     start.className = "started";
 
@@ -23,16 +36,25 @@ function startGame(){
     const quoteText = quotes[quoteIndex];
 
     // quoteText = "type me";
-    wordQueue = quoteText.split(' ');
+    wordQueue = removeSpecialChars(quoteText).split(' ');
     quote.innerHTML = wordQueue.map(word => (`<span>${word}</span>`)).join('');
 
     highlightPosition = 0;
     quote.childNodes[highlightPosition].className = 'highlight';
 
     startTime = new Date().getTime();
+    
+    document.body.className = "";
+    start.className = "started";
+    setTimeout(() => {start.className = "button";}, 2000);
 }
-start.addEventListener('click', startGame);
-input.addEventListener('input', checkInput);
+
+
+function removeSpecialChars(str) {
+    return str.replace(/(?!\w|\s)./g, '')
+      .replace(/\s+/g, ' ')
+      .replace(/^(\s*)([\W\w]*)(\b\s*$)/g, '$2');
+  }
 
 function checkInput() {
     console.log("Checking", input.value);
@@ -57,10 +79,46 @@ function checkInput() {
 }
 
 function gameOver(){
+
     const elapsedTime = new Date().getTime() - startTime;
     document.body.className = "winner";
     message.innerHTML = `<span class="congrats">Congratulations!</span>
     <br>
     You finished in ${elapsedTime/1000} seconds`;
 
+    const lastScoreItem = scores.pop();
+    lastScoreItem.milliseconds = elapsedTime;
+    scores.push(lastScoreItem);
+    saveScores();
+
+    //clear out the list
+    while(scoresUnorderedList.firstChild){
+        scoresUnorderedList.removeChild(scoresUnorderedList.firstChild);
+    }
+
+    for(let score of getScores()){
+        const li = createElementForScore(score);
+        scoresUnorderedList.appendChild(li);
+    }
+
+}
+
+function getScores(){
+    const noScoreFound = "[]";
+    const scoresJSON = localStorage.getItem('scores') || noScoreFound;
+    return JSON.parse(scoresJSON);
+}
+
+function saveScores(){
+    const data = JSON.stringify(scores);
+    localStorage.setItem('scores', data);
+}
+
+function createElementForScore(score){
+    const template = document.getElementById("score-item-template");
+    const newListItem = template.content.cloneNode(true);
+
+    const text = newListItem.querySelector(".score-text");
+    text.innerHTML = score.name + " in " + score.milliseconds/1000 + " seconds.";
+    return newListItem;
 }
